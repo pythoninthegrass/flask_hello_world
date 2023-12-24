caddf25509f6a56226674d4d606e07982158e928
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-12-23 21:05:57 -0600

    Render html template
    Render index via tailwind css

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/hello')
+def hello():
     return 'Hello, World!'
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
